325725a31b1a27a698d468d4a696d596d477bf08
CodeF0x <contact@codef0x.dev>
2019-11-14 14:30:04 +0100

    Header component ist now customizable.

--- components/header/header.js
+++ components/header/header.js
@@ -6,19 +6,21 @@ class AppHeader extends HTMLElement {
   }
 
   connectedCallback() {
+    const title = this.getAttribute('title');
     const template = `
-      <script src="${JAVASCRIPT}"></script>
-      <style>
-        ${SHADOW_CSS};
-      </style>
+    <script src="${JAVASCRIPT}"></script>
+    <style>
+    ${SHADOW_CSS};
+    </style>
       <header class="mdc-top-app-bar">
         <div class="mdc-top-app-bar__row">
           <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
             <button class="material-icons mdc-top-app-bar__navigation-icon mdc-icon-button">menu</button>
-            <span class="mdc-top-app-bar__title">Title</span>
+            <span class="mdc-top-app-bar__title">${title}</span>
           </section>
+          <section id="links" class="mdc-top-app-bar__section mdc-top-app-bar__section--align-end"></section>
         </div>
-      </header>
+    </header>
     `;
 
     const templateContent = document.createElement('div');
@@ -27,6 +29,24 @@ class AppHeader extends HTMLElement {
     const shadowRoot = this.attachShadow({ mode: 'open' }).appendChild(
       templateContent.cloneNode(true)
     );
+
+    this.createLinks(shadowRoot);
+  }
+
+  createLinks(shadowRoot) {
+    const linksText = this.getAttribute('link-titles').split(',');
+    const linksHref = this.getAttribute('link-hrefs').split(',');
+
+    console.log(shadowRoot);
+    const section = shadowRoot.querySelector('#links');
+
+    linksText.forEach((text, index) => {
+      const link = document.createElement('a');
+      link.innerText = text;
+      link.href = linksHref[index];
+
+      section.appendChild(link);
+    });
   }
 }
 
