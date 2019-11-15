6c61c0b11a3965ac7419407c92211be2e0bfb95c
CodeF0x <contact@codef0x.dev>
2019-11-15 21:55:41 +0100

    Adjustments to number card and numbers site.

--- components/number-card/number-card.js
+++ components/number-card/number-card.js
@@ -20,38 +20,27 @@ class AppNumberCard extends HTMLElement {
       text = this.getAttribute('text');
 
     const template = `
-    <script src="${JAVASCRIPT}"></script>
     <style>
     ${SHADOW_CSS}
 
-    .demo-card {
-      width: 350px;
-      margin: 10px 0 10px 0;
+    .w3-card {
+      width: 400px;
+      margin: 0 10px;
+      padding: 0;
     }
 
-    .mdc-typography--headline6,
-    .mdc-typography--subtitle2,
-    .mdc-typography--body2 {
-      padding: 0 10px;
+    .phone-card {
+      padding: 10px;
+      margin: 10px 0;
     }
     </style>
-
-    <div class="mdc-card__primary-action demo-card__primary-action" tabindex="0">
-      <div class="demo-card__primary">
-        <h2 class="demo-card__title mdc-typography mdc-typography--headline6">${phoneNumber}</h2>
-        <h3 class="demo-card__subtitle mdc-typography mdc-typography--subtitle2">${institution}</h3>
-      </div>
-      <div class="demo-card__secondary mdc-typography mdc-typography--body2">${text}</div>
-    </div>
-    <div class="mdc-card__actions">
-      <div class="mdc-card__action-icons">
-        <a class="mdc-icon-button material-icons mdc-card__action mdc-card__action--icon--unbounded" title="More options" data-mdc-ripple-is-unbounded="true" href="tel:${phoneNumber}">phone</a>
-      </div>
-    </div>
+    <h2>${phoneNumber}<h2>
+    <h3>${institution}</h3>
+    <p>${text}</p>
     `;
 
     const templateContent = document.createElement('div');
-    templateContent.classList.add('mdc-card', 'demo-card');
+    templateContent.classList.add('w3-panel', 'w3-card', 'phone-card');
     templateContent.innerHTML = template;
 
     const shadowRoot = this.attachShadow({ mode: 'open' }).appendChild(
