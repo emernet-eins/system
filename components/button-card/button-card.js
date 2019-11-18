1eea84da8bd0725a49f8abb43915e39dbf1a23de
CodeF0x <contact@codef0x.dev>
2019-11-18 14:55:16 +0100

    Added card component with button.

---
+++ components/button-card/button-card.js
@@ -0,0 +1,97 @@
+import { SHADOW_CSS } from '../../src/js/constants.js';
+
+/**
+ * Card that serves as navigation and info element.
+ *
+ * Attributes:
+ *
+ * image-url: Url of background-image
+ * title: Title of the card
+ * subtitle: Subtitle of the card
+ * text: Text in card body
+ * button-text: Text of button
+ * button-link: Link of button
+ */
+class AppButtonCard extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    const imageUrl = this.getAttribute('image-url'),
+      title = this.getAttribute('title'),
+      subtitle = this.getAttribute('subtitle'),
+      text = this.getAttribute('text');
+
+    const template = `
+        <style>
+        ${SHADOW_CSS}
+
+        .w3-card {
+          width: 400px;
+          margin: 0 10px;
+          padding: 0;
+        }
+
+        .card-background-image {
+          height: 200px;
+          background-size: cover;
+          display: flex;
+          align-content: end;
+          color:
+          #fff;
+          padding: 10px;
+        }
+
+        .card-background-image > h2 {
+          align-self: end;
+        }
+
+        .w3-card > p {
+          padding: 10px;
+        }
+
+        .button-card-mid {
+            display: flex;
+            justify-content: center;
+        }
+
+        .button-card-mid > a {
+            margin: 5px;
+        }
+        </style>
+        ${
+          imageUrl
+            ? `<div class="card-background-image" style="background-image: url('${imageUrl}');"><h2>${title}</h2></div>`
+            : `<h2>${title}</h2>`
+        }
+        <h3>${subtitle}</h3>
+        <p>${text}</p>
+        <div class="button-card-mid"></div>
+    `;
+
+    const templateContent = document.createElement('div');
+    templateContent.classList.add('w3-panel', 'w3-card');
+    templateContent.innerHTML = template;
+
+    const shadowRoot = this.attachShadow({ mode: 'open' }).appendChild(
+      templateContent.cloneNode(true)
+    );
+
+    this.createButton(shadowRoot);
+  }
+
+  createButton(shadowRoot) {
+    const buttonText = this.getAttribute('button-text'),
+      buttonLink = this.getAttribute('button-link');
+
+    const button = document.createElement('a');
+    button.href = buttonLink;
+    button.innerText = buttonText;
+    button.classList.add('w3-button', 'w3-ripple', 'w3-teal');
+
+    shadowRoot.querySelector('.button-card-mid').appendChild(button);
+  }
+}
+
+export default AppButtonCard;
--- src/js/component-registration.js
+++ src/js/component-registration.js
@@ -6,12 +6,14 @@
 import AppHeader from '../../components/header/header.js';
 import AppCard from '../../components/card/card.js';
 import AppNumberCard from '../../components/number-card/number-card.js';
+import AppButtonCard from '../../components/button-card/button-card.js';
 
 const registerComponents = () => {
   const components = [
     { componentName: 'app-header', class: AppHeader },
     { componentName: 'app-card', class: AppCard },
-    { componentName: 'app-number-card', class: AppNumberCard }
+    { componentName: 'app-number-card', class: AppNumberCard },
+    { componentName: 'app-button-card', class: AppButtonCard }
   ];
 
   components.forEach(component => {
